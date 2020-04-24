9a621d6c4b8f361988de88e73251a9dad25bdb12
rajauzairabdullah <Dukandar12!@>
2020-04-24 19:31:54 +0500

    First Commit...

--- app/src/main/java/app/fyp/sibtainfyp/ui/home/HomeFragment.java
+++ app/src/main/java/app/fyp/sibtainfyp/ui/home/HomeFragment.java
@@ -1,49 +1,19 @@
 package app.fyp.sibtainfyp.ui.home;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import app.fyp.sibtainfyp.R;
 
-public class HomeFragment extends Fragment implements OnMapReadyCallback {
-
+public class HomeFragment extends Fragment {
     private HomeViewModel homeViewModel;
-    GoogleMap map;
-    Location currentlocation;
-    FusedLocationProviderClient fusedLocationProviderClient;
-    double lat;
-    double lon;
-    private static final int REQUEST_CODE = 110;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -62,58 +32,13 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
             }
         });
 
-//        final TextView textView = root.findViewById(R.id.text_home);
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
-
-
-        //Location work
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
-
-        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if(location != null){
-                    Log.v("notnull","loc is not null");
-                    Log.v("loc",String.valueOf(location));
-                }else{
-                 Log.v("null","loc is null");
-                }
-            }
-        });
-        //Location work
-
-
-
-
 
         return root;
     }
 
 
 
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        supportMapFragment.getMapAsync(this);
-    }
 
 
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-
-        LatLng latlng = new LatLng(lat,lon);
-        MarkerOptions markerOptions = new MarkerOptions().position(latlng).title("Current Location");
-        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latlng));
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng,5));
-        googleMap.addMarker(markerOptions);
-    }
 
 }
